1707feac7ee917f6a902113be19c22882d3383e6
reinhardtken <reinhardtken@hotmail.com>
2023-08-09 17:51:10 +0800

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module browser-test
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.4.0
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/mattn/go-isatty v0.0.7 // indirect
+)
--- src/main/main.go
+++ src/main/main.go
@@ -1,18 +1,19 @@
 package main
 
-
 import (
-
-"github.com/gin-gonic/gin"
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
+	"path/filepath"
 )
-
 
 /*
 build
 export GOARCH=amd64
 export GOOS=linux
 go build -o aldServer  ./server.go
+/Users/bytedance/sdk/go1.19.5/bin/go  build -o webXRServer  ./main.go
 
 set path=C:\Go1.12\bin;%path%
 set GOROOT=C:\Go1.12
@@ -22,6 +23,12 @@ set GOOS=linux
 go build -o aldServer  ./server.go
 */
 
+func getCurrentPath() string {
+	if ex, err := os.Executable(); err == nil {
+		return filepath.Dir(ex)
+	}
+	return "./"
+}
 
 func main() {
 
@@ -36,11 +43,27 @@ func main() {
 	//mac
 	router.Static("/static/", `/Users/bytedance/workspace/code/self/browser-test/html`)
 	router.Static("/js", `/Users/bytedance/workspace/code/self/browser-test/html/js`)
+	router.Static("/webxr/", `/Users/bytedance/workspace/code/self/browser-test/html/webxr-samples`)
+	router.Static("/webgl/", `/Users/bytedance/workspace/code/self/browser-test/html/webglbook`)
+	router.Static("/webgl2/", `/Users/bytedance/workspace/code/self/browser-test/html/WebGL2Samples`)
+
+	//linux
+	//router.Static("/static/", `/home/liuqingping/workspace/tmp/weblayer_test/html`)
+	//router.Static("/js", `/home/liuqingping/workspace/tmp/weblayer_test/html/js`)
+	//router.Static("/webxr/", `/home/liuqingping/workspace/tmp/weblayer_test/html/webxr`)
+
+	//pwd
+	//path := getCurrentPath()
+	//fmt.Printf("webxrtest server path %s \n", path)
+	//router.Static("/static/", path+`/html`)
+	//router.Static("/js", path+`/html/js`)
+	//router.Static("/webxr/", path+`/html/webxr`)
+
 	router.GET("/hello",
 		func(context *gin.Context) {
 			context.String(http.StatusOK, "<p>hello world</p>")
 		})
 
-	router.Run(":8800")
+	fmt.Printf("webxrtest server run...")
+	router.Run(":80")
 }
-
